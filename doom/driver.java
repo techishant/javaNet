ac9ca34b9397f9a3495443f388217e6a48c4a2bb
Ishant Sharma <ishantsh12234@gmail.com>
2023-03-12 12:00:03 +0530

    morning upd

--- doom/driver.java
+++ doom/driver.java
@@ -1,11 +1,21 @@
 package doom;
 
+import java.util.Scanner;
+
 public class driver {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         client myclient  = new client();
         myclient.startConnection("127.0.0.1", 4444);
-        String response = myclient.sendMessage("hello server");
-        System.out.println(response);
+        while(true){
+            String inp = in.nextLine();
+            String response = myclient.sendMessage(inp);
+            System.out.println(response);
+            if(inp.equals("0")){
+                break;
+            }
+        }
+        
 
     }
 }
--- doom/host.java
+++ doom/host.java
@@ -19,22 +19,51 @@ public class host {
         Myhost.start(9991);
     }
 
+    public int getInput(Scanner in, PrintWriter out){
+        String greeting = in.nextLine();
+        if("hello server".equals(greeting)){
+            out.println("hello client");
+        }
+        else{
+            out.println("unrecognised greeting");
+        }
+        if("0".equals(greeting)){
+            System.out.println("00000");
+            return 0;
+        }
+        return 1;
+    }
+
     public void start(int port){
         try {
             serverSocket = new ServerSocket(port);
             clientSocket = serverSocket.accept();
             out = new PrintWriter(clientSocket.getOutputStream(), true);
             in = new Scanner(clientSocket.getInputStream(), "UTF-8");
-            String greeting = in.nextLine();
-            if("hello server".equals(greeting)){
-                out.println("hello client");
-            }
-            else{
-                out.println("unrecognised greeting");
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        System.out.println("----------------------");
+        System.out.println("Local Port: " + serverSocket.getLocalPort());
+        System.out.println("Channel: " + serverSocket.getChannel());
+        System.out.println("Inet Address: " + serverSocket.getInetAddress());
+        System.out.println("Local Socket Address: " + serverSocket.getLocalSocketAddress());
+        System.out.println("Server Started Successfully");
+
+        
+
+        int st;
+        while(true){
+            st = getInput(in, out);
+            if (st==0) break;
+        }
+        System.out.println("Outside loop( st: " + st);
+
+        stop();
+        System.out.println("Server stopped");
+        System.exit(0);
     }
 
     public void stop(){
